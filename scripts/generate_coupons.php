67900a0d91adb90345d3296279f900f0b203faed
Mixerist <61286875+Mixerist@users.noreply.github.com>
2022-08-31 21:53:09 +0300

    added coupon generation script

---
+++ scripts/generate_coupons.php
@@ -0,0 +1,17 @@
+<?php
+
+$config = require_once "C:\OpenServer\domains\discordbot\config.php";
+
+$pdo = new PDO("sqlsrv:Server={$config['server_ip']};Database={$config['database']}", $config['username'], $config['password']);
+
+/*
+ * Не создавайте одновременно очень много купонов чтобы не положить базу.
+ */
+$count = 100; /* Количество купонов */
+$denomination = 200; /* Номинал купона */
+
+for ($i = 0; $i < $count; $i++) {
+    $pdo->query("INSERT INTO [FNLBilling].[dbo].[coupons] (coupon, denomination) VALUES (REPLACE(NEWID(), '-', ''), $denomination)");
+}
+
+echo "$count codes successfully generated.";
