2320bcdd0c7640660c5b1f22e4627ee60797ae7a
Mixerist <61286875+Mixerist@users.noreply.github.com>
2023-01-29 22:43:06 +0300

    Update Gift.php

--- app/Handlers/Gift.php
+++ app/Handlers/Gift.php
@@ -6,6 +6,7 @@ use Discord\Builders\MessageBuilder;
 use Discord\Parts\Interactions\Interaction;
 use Exception;
 use PDO;
+use PDOException;
 
 class Gift
 {
@@ -13,7 +14,11 @@ class Gift
 
     private Interaction $interaction;
 
+    private ?int $user_id = null;
+
     private array $promo_code = [];
+
+    private ?int $max_character_level = null;
 
     public function __construct(PDO $pdo, Interaction $interaction)
     {
@@ -24,14 +29,18 @@ class Gift
     public function run()
     {
         try {
-            $user_id = $this->getUserId($this->interaction->data->options['login']['value']);
-            $this->promo_code = $this->getPromoCode($this->interaction->data->options['promo_code']['value']);
-            $max_character_level = $this->getMaxCharacterLvl($this->interaction->data->options['login']['value']);
-            $this->isPromoCodeUsed($user_id, $this->promo_code['id']);
-            $this->checkMinLvl($this->promo_code['min_lvl'], $max_character_level);
+            $this->user_id = $this->getUserId();
+            $this->promo_code = $this->getPromoCode();
+            $this->max_character_level = $this->getMaxCharacterLvl();
 
-            if ((int)$this->promo_code['max_lvl'] > 0) {
-                $this->checkMaxLvl($this->promo_code['max_lvl'], $max_character_level);
+            $this->isPromoCodeUsed();
+
+            if ($this->promo_code['min_lvl'] > 0) {
+                $this->checkMinLvl();
+            }
+
+            if ($this->promo_code['max_lvl'] > 0) {
+                $this->checkMaxLvl();
             }
 
         } catch (Exception $e) {
@@ -43,20 +52,20 @@ class Gift
 
             $sql = $this->pdo->prepare("INSERT INTO [FNLBilling].[dbo].[redeemed_promo_codes] (user_id, promo_code_id) VALUES (:user_id, :promo_code_id)");
             $sql->execute([
-                'user_id' => $user_id,
+                'user_id' => $this->user_id,
                 'promo_code_id' => $this->promo_code['id']
             ]);
 
             $sql = $this->pdo->prepare("INSERT INTO [FNLBilling].[dbo].[TBLSysOrderList] (mAvailablePeriod, mCnt, mItemID, mPracticalPeriod, mSvrNo, mSysID, mUserNo, mBindingType, mLimitedDate, mItemStatus) SELECT items.available_period, items.count, items.item_id, items.practical_period, promo_codes.server_id, promo_codes.sys_id, :user_id, items.binding_type, promo_codes.limited_date, items.item_status FROM [FNLBilling].[dbo].[promo_codes] INNER JOIN [FNLBilling].[dbo].[items] ON promo_codes.id = items.promo_code_id WHERE promo_code = :promo_code");
             $sql->execute([
                 'promo_code' => $this->promo_code['promo_code'],
-                'user_id' => $user_id
+                'user_id' => $this->user_id
             ]);
 
             $this->pdo->commit();
 
             return $this->interaction->respondWithMessage(MessageBuilder::new()->setContent('Промокод успешно активирован! Проверьте свои подарки.'));
-        } catch (\PDOException $e) {
+        } catch (PDOException $e) {
             $this->pdo->rollBack();
 
             return $this->interaction->respondWithMessage(MessageBuilder::new()->setContent('Что-то пошло не так. Попробуйте снова.'));
@@ -66,10 +75,10 @@ class Gift
     /**
      * @throws Exception
      */
-    private function getUserId(string $login)
+    private function getUserId()
     {
         $sql = $this->pdo->prepare("SELECT mUserNo FROM [FNLAccount].[dbo].[TblUser] WHERE mUserId = :login");
-        $sql->execute(['login' => $login]);
+        $sql->execute(['login' => $this->interaction->data->options['login']['value']]);
 
         if ($result = $sql->fetch(PDO::FETCH_ASSOC)['mUserNo']) {
             return $result;
@@ -81,10 +90,10 @@ class Gift
     /**
      * @throws Exception
      */
-    private function getPromoCode(string $promoCode)
+    private function getPromoCode()
     {
         $sql = $this->pdo->prepare("SELECT * FROM [FNLBilling].[dbo].[promo_codes] WHERE promo_code = :promo_code AND limited_date > GETDATE() AND is_enabled = 1");
-        $sql->execute(['promo_code' => $promoCode]);
+        $sql->execute(['promo_code' => $this->interaction->data->options['promo_code']['value']]);
 
         if ($result = $sql->fetch(PDO::FETCH_ASSOC)) {
             return $result;
@@ -96,12 +105,12 @@ class Gift
     /**
      * @throws Exception
      */
-    private function getMaxCharacterLvl(string $login)
+    private function getMaxCharacterLvl()
     {
         $server = $this->defineServer();
 
         $sql = $this->pdo->prepare("SELECT MAX(mLevel) AS mLevel FROM [FNLAccount].[dbo].[TblUser] INNER JOIN [{$server['database_name']}].[dbo].[TblPc] ON TblUser.mUserNo = TblPc.mOwner INNER JOIN [{$server['database_name']}].[dbo].[TblPcState] ON TblPc.mNo = TblPcState.mNo WHERE mUserId = :login AND TblPc.mDelDate IS NULL");
-        $sql->execute(['login' => $login]);
+        $sql->execute(['login' => $this->interaction->data->options['login']['value']]);
 
         if ($max_character_lvl = $sql->fetch(PDO::FETCH_ASSOC)['mLevel']) {
             return $max_character_lvl;
@@ -117,9 +126,9 @@ class Gift
     {
         $server_id = $this->promo_code['server_id'];
 
-        foreach (config()['servers'] as $item) {
-            if ($item['server_id'] == $server_id) {
-                return $item;
+        foreach (config()['servers'] as $server) {
+            if ($server['server_id'] == $server_id) {
+                return $server;
             }
         }
 
@@ -129,12 +138,12 @@ class Gift
     /**
      * @throws Exception
      */
-    private function isPromoCodeUsed($user_id, $promo_code_id)
+    private function isPromoCodeUsed()
     {
         $sql = $this->pdo->prepare("SELECT COUNT(*) FROM [FNLBilling].[dbo].[redeemed_promo_codes] WHERE user_id = :user_id AND promo_code_id = :promo_code_id");
         $sql->execute([
-            'user_id' => $user_id,
-            'promo_code_id' => $promo_code_id
+            'user_id' => $this->user_id,
+            'promo_code_id' => $this->promo_code['id']
         ]);
 
         if ($sql->fetchColumn()) {
@@ -145,21 +154,20 @@ class Gift
     /**
      * @throws Exception
      */
-    private function checkMinLvl($min_lvl, $max_character_lvl)
+    private function checkMinLvl()
     {
-        if ($max_character_lvl < $min_lvl) {
-            throw new Exception("Ваш уровень слишком низок чтобы использовать этот промокод. Минимальный уровень использования: $min_lvl.");
+        if ($this->max_character_level < $this->promo_code['min_lvl']) {
+            throw new Exception("Ваш уровень слишком низок чтобы использовать этот промокод. Минимальный уровень использования: {$this->promo_code['min_lvl']}.");
         }
     }
 
     /**
      * @throws Exception
      */
-    private function checkMaxLvl($max_lvl, $max_character_lvl)
+    private function checkMaxLvl()
     {
-        if ($max_character_lvl > $max_lvl) {
-            throw new Exception("Ваш уровень слишком высок чтобы использовать этот промокод. Максимальный уровень использования: $max_lvl.");
+        if ($this->max_character_level > $this->promo_code['max_lvl']) {
+            throw new Exception("Ваш уровень слишком высок чтобы использовать этот промокод. Максимальный уровень использования: {$this->promo_code['max_lvl']}.");
         }
-
     }
 }
